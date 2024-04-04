6f391c4fc5f91b2317d3e276d7e28f6d1a258acc
Evandro Lugli <evandro.lugli@gmail.com>
2024-04-04 15:43:50 +1100

    Add video upload

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from 'fastify'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideosRoute } from './routes/upload-videos'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
+app.register(uploadVideosRoute)
 
 app.listen({
     port: 3333,
